a351f5b7784f735a3cf01fcf9434bd226e88d868
MarkRywell <gopro855@gmail.com>
2022-12-06 22:30:01 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:qr_scanner_itlympics/views/homepage.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage()
+      ));
+}
+

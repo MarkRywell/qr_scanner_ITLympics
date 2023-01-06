5adc74d8d745ef364edcacc682a3260374fb5fdb
Mark Rywell Gaje <gopro855@gmail.com>
2023-01-06 22:11:36 +0800

    Updated data page to display proper data

--- lib/views/data_page.dart
+++ lib/views/data_page.dart
@@ -1,42 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner_itlympics/custom_widgets/custom_text.dart';
+import 'package:qr_scanner_itlympics/models/api.dart';
+import 'package:qr_scanner_itlympics/models/student.dart';
 
 class DataPage extends StatefulWidget {
 
-  final String value;
+  final Student student;
   final Function screenClosed;
 
-    const DataPage({
-      required this.value,
-      required this.screenClosed,
-      Key? key}) : super(key: key);
+  const DataPage({
+    required this.student,
+    required this.screenClosed,
+    Key? key}) : super(key: key);
 
   @override
   State<DataPage> createState() => _DataPageState();
 }
 
 class _DataPageState extends State<DataPage> {
+
+
+  showStatus({required Color color, required String text}) {    // Snackbar to show message of API Response
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(text),
+            backgroundColor: color,
+            padding: const EdgeInsets.all(15),
+            behavior: SnackBarBehavior.fixed,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            )
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("QR Code Data"),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_outlined),
-          onPressed: () {
-            widget.screenClosed();
-            Navigator.pop(context);
-          },
-        )
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10),
+        appBar: AppBar(
+            title: const Text("QR Code Data"),
+            centerTitle: true,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_outlined),
+              onPressed: () {
+                widget.screenClosed();
+                Navigator.pop(context);
+              },
+            )
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(10),
           child: Column(
             children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Text("QR Code Data",
+                  style: TextStyle(
+                      fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                  constraints: BoxConstraints(
+                    minHeight: size.height * 0.2,
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2)
+                  ),
+                  child: Column(
+                    children: [
+                      CustomText(textTitle: "Student ID: ", textData: widget.student.id.toString()),
+                      CustomText(textTitle: "Student Name: ", textData: widget.student.name),
+                      CustomText(textTitle: "Course: ", textData: widget.student.course),
+                      CustomText(textTitle: "Year: ", textData: widget.student.year.toString())
+                    ],
+                  ),
+              ),
+              const SizedBox(height: 100),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 40,
+                  width: size.width * 0.6,
+                  child: ElevatedButton(
+                    onPressed: () async {
 
+                     var response = Api.instance.addStudent(widget.student);
+
+                     if(response == 201) {
+                       showStatus(color: Colors.greenAccent, text: "Student Added");
+                       Navigator.pop(context);
+                     }
+                     else {
+                       showStatus(color: Colors.redAccent, text: "Attendance Error");
+                     }
+
+                    },
+                    child: const Text("ADD STUDENT",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14
+                    ),),
+                  ),
+                ),
+              )
             ],
           ),
         )

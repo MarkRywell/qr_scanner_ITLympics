158b4f829146a37316e715d4e6a54ff657531325
Mark Rywell Gaje <gopro855@gmail.com>
2023-02-07 17:42:01 +0800

    Added day attribute

--- lib/models/student.dart
+++ lib/models/student.dart
@@ -1,12 +1,14 @@
 class Student {
 
-  final int id;
+  final String day;
+  final student_id;
   final String name;
   final String course;
   final int year;
 
   Student({
-    required this.id,
+    required this.day,
+    required this.student_id,
     required this.name,
     required this.course,
     required this.year
